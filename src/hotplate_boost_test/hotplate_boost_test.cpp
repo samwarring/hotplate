1c5a85bea1a55bc129d4e26766c5538772404709
samwarring <samwarring@gmail.com>
2019-03-24 17:42:56 -0700

    Add boost unit test
    New binary and new dependency. Ignoring dependency management for now - let's just see what's installed on the build machine.

---
+++ src/hotplate_boost_test/hotplate_boost_test.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE hotplate_boost_test
+#include <boost/test/unit_test.hpp>
+#include <hotplate/hotplate.h>
+
+BOOST_AUTO_TEST_SUITE()
+
+BOOST_AUTO_TEST_CASE(hotplate_test_1)
+{
+	hotplate::hotplate hp(42);
+	BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
