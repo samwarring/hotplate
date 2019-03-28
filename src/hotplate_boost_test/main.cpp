ef7c23996beeab26801f42bfb1dbff0f3605a864
samwarring <samwarring@gmail.com>
2019-03-28 02:04:23 -0700

    Support for boost tests across multiple files

---
+++ src/hotplate_boost_test/main.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE hotplate_boost_test
+#include <boost/test/unit_test.hpp>
--- src/hotplate_boost_test/substance_boost_test.cpp
+++ src/hotplate_boost_test/substance_boost_test.cpp
@@ -1,12 +1,13 @@
 #include <boost/test/unit_test.hpp>
 #include <hotplate/hotplate.h>
 
-BOOST_AUTO_TEST_SUITE(hotplate_test_suite)
+BOOST_AUTO_TEST_SUITE(substance_test_suite)
 
-BOOST_AUTO_TEST_CASE(hotplate_test_1)
+BOOST_AUTO_TEST_CASE(substance_test_1)
 {
-	hotplate::hotplate hp(42);
+	hotplate::substance s(50, 0, 100);
 	BOOST_REQUIRE(true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
