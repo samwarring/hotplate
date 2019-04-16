b03325c06cabac4385b6f96e31807033834d4e88
Sam <samwarring@gmail.com>
2019-04-16 05:20:53 +0000

    Fixed some issues in travis before_build script

--- scripts/travis_before_script.sh
+++ scripts/travis_before_script.sh
@@ -5,19 +5,20 @@ mkdir build
 cd build
 
 # If compiling for x86 linux, set up the cross-compilation environment
-uname -a | grep OSX >/dev/null
-if [[ $? = 0 and $ARCH = x86 ]]; then
+uname -a | grep Linux >/dev/null
+export IS_LINUX=$?
+if [ $IS_LINUX = 0 ] && [ $ARCH = x86 ]; then
     echo Setting up cross compilation for x86 linux from x64 linux...
     sudo dpkg --add-architecture i386
     sudo apt update
     sudo apt install -y g++-multilib libboost-all-dev:i386
     cmake .. -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain_x86_linux_on_x64_linux.cmake
-elif [[ $? = 0 and $ARCH = x64 ]]; then
+elif [ $IS_LINUX = 0 ] && [ $ARCH = x64 ]; then
     echo Setting up native x64 Linux compilation environment...
-    sudo apt install libboost-all-dev
-    cmake .. -DCMAKE_BUILD_TUPE=$CONFIG
+    sudo apt install -y libboost-all-dev
+    cmake .. -DCMAKE_BUILD_TYPE=$CONFIG
 else
     echo Setting up native x64 Mac compilation environment...
-    cmake .. -DCMAKE_BUILD_TUPE=$CONFIG
+    cmake .. -DCMAKE_BUILD_TYPE=$CONFIG
 fi
 
