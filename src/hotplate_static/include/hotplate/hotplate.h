60be77986676c71ed259d144727535e322b0c703
samwarring <samwarring@gmail.com>
2019-09-01 17:01:48 -0700

    Set up Doxygen
    I added some docstrings too, but they are just placeholders right now. (Hopefully) I'll come back and make it more accurate.

--- src/hotplate_static/include/hotplate/hotplate.h
+++ src/hotplate_static/include/hotplate/hotplate.h
@@ -1,17 +1,34 @@
 #ifndef _HOTPLATE_H_
 #define _HOTPLATE_H_
 
+/**
+ * \brief This is the brief description
+ * 
+ * This is the root namespace for all APIs defined by the Hotplate library.
+ * Having a root namespace like this for an external library is a good idea
+ * because it prevents our API names from clashing with other external 
+ * libraries.
+ * 
+ *   \warning  This is a warning
+ *   \note This is a note
+ */
 namespace hotplate
 {
+	/**
+	 * The state of matter for a substance
+	 * \warning this is a warning in an enum description
+	 */
 	enum class substance_state
 	{
-		solid,
-		liquid,
-		gas
+		solid,   ///< The substance is a solid.
+		liquid,  ///< The substance is a liquid.
+		gas      ///< The substance is a gas.
 	};
 
 	
-	// Substances can be "put on" hotplates and react differently.
+	/** Substances can be "put on" hotplates and react differently.
+	 * \warning this is a warning on a class description
+	 */
 	class substance
 	{
 	private:
@@ -27,7 +44,8 @@ namespace hotplate
 	public:
 		substance(int current_temperature, int melting_temperature, int boiling_temperature) noexcept;
 
-		// Gets the current temperature.
+		/// Gets the current temperature.
+		/// \warning this is a warning on a method description
 		auto get_temperature() const noexcept -> int;
 
 		// Sets the current temperature.
@@ -38,7 +56,8 @@ namespace hotplate
 	};
 	
 
-	// Hotplates heat up substances.
+	/** Hotplates heat up substances.
+	 */
 	class hotplate
 	{
 	private:
@@ -51,14 +70,18 @@ namespace hotplate
 	public:
 		hotplate(int current_temperature) noexcept;
 
-		// Sets the current temperature (in Kelvin)
+		//! Sets the current temperature (in Kelvin)
+		// \param temperature temperature in Kelvin
 		auto set_temperature(int temperature) noexcept -> void;
 
-		// Puts a new substance on the hotplate. Returns false if
-		// the hotplate is already in use.
+		/** Puts a new substance on the hotplate. Returns false if
+		 * the hotplate is already in use.
+		 * 
+		 * \param subst The substance to heat on the hotplate.
+		 */
 		auto heat_substance(substance& subst) noexcept -> bool;
 
-		// Removes the substance from the hotplate.
+		//! Removes the substance from the hotplate.
 		auto remove_substance() noexcept -> void;
 	};
 }
